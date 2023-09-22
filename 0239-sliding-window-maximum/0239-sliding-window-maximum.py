98a5776906c81efd80ddc6eb2063b0277921525c
Nikilesh <115042113+n11kkk@users.noreply.github.com>
2023-09-22 18:39:13 -0500

    Time: 1374 ms (36.87%), Space: 33.1 MB (69.97%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,6 +1,30 @@
 from collections import deque
+# from queue import PriorityQueue  
 class Solution:
     def maxSlidingWindow(self, nums: List[int], slw: int) -> List[int]:
+        # def getLast(prq,slw):
+        #     count = 0
+        #     prq1 = PriorityQueue()
+        #     while(count<slw-1):
+        #         toAdd = prq.get()[1]
+        #         prq1.put((-toAdd,toAdd))
+        #         count+=1
+        #     return prq1
+        # prq = PriorityQueue()
+        # ans = []
+        # for i in range(len(nums)):
+        #     prq.put((-nums[i],nums[i]))
+        #     if(prq.qsize()>=slw):
+        #         anss = prq.get()[1]
+        #         # print(prq)
+        #         ans.append(anss)
+        #         prq.put((-anss,anss))
+        #         prq = getLast(prq,slw)
+        # return ans
+                
+                
+            
+        
         deq = deque()
         ans=[]
         for i in range(len(nums)):
@@ -16,3 +40,6 @@ class Solution:
             if(i>=slw-1):
                 ans.append(nums[deq[0]])
         return ans
+
+        
+    

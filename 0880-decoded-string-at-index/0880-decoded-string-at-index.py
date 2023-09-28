87f4fd3a23ed9bec454fa75df0545c0da52da594
Nikilesh <115042113+n11kkk@users.noreply.github.com>
2023-09-28 00:28:17 -0500

    Time: 46 ms (6.03%), Space: 16.2 MB (85.93%) - LeetHub

--- 0880-decoded-string-at-index/0880-decoded-string-at-index.py
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.py
@@ -1,18 +1,36 @@
 class Solution:
     def decodeAtIndex(self, s: str, k: int) -> str:
-        currCount = 0
-        for i in range(len(s)):
-            if(s[i].isdigit()):
-                currCount+=currCount*((int(s[i])-1))
+        # currCount = 0
+        # for i in range(len(s)):
+        #     if(s[i].isdigit()):
+        #         currCount+=currCount*((int(s[i])-1))
+        #     else:
+        #         currCount+=1
+        # for i in range(len(s)-1,-1,-1):
+        #     if(s[i].isdigit()):
+        #         # print(currCount,k)
+        #         currCount=currCount/int(s[i])
+        #         k=k%currCount
+        #         # print(currCount,k)
+        #     else:
+        #         if(currCount==k or k==0):
+        #             return s[i]
+        #         currCount-=1
+        
+        
+        count = 0
+        for i in s:
+            if(i.isdigit()):
+                count=count*int(i)
             else:
-                currCount+=1
+                count+=1
         for i in range(len(s)-1,-1,-1):
             if(s[i].isdigit()):
-                # print(currCount,k)
-                currCount=currCount/int(s[i])
-                k=k%currCount
-                # print(currCount,k)
+                # print("IF",count,k)
+                count=count/int(s[i])
+                k=k%count
             else:
-                if(currCount==k or k==0):
+                # print("ELSE",count,k)
+                if(count==k or k==0):
                     return s[i]
-                currCount-=1
+                count-=1

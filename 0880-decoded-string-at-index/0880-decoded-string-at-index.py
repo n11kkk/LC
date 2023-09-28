f36385738beedd55baa8bde5fd1e405f94397f19
Nikilesh <115042113+n11kkk@users.noreply.github.com>
2023-09-28 00:13:54 -0500

    Time: 44 ms (15.08%), Space: 16.2 MB (85.93%) - LeetHub

--- 0880-decoded-string-at-index/0880-decoded-string-at-index.py
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.py
@@ -1,24 +1,18 @@
 class Solution:
-    # def decodeAtIndex(self, s: str, k: int) -> str:
-    #     ans = ""
-    #     for i in range(len(s)):
-    #         if(len(ans)>=k):
-    #             return ans[k-1]
-    #         if(s[i].isdigit()):
-    #             ans+=ans*(int(s[i])-1)
-    #         else:
-    #             ans+=s[i]
-    #     return ans[k-1]
-    def decodeAtIndex(self, S, K):
-        N = 0
-        for i, c in enumerate(S):
-            N = N * int(c) if c.isdigit() else N + 1
-            if K <= N: break
-        for j in range(i, -1, -1):
-            c = S[j]
-            if c.isdigit():
-                N /= int(c)
-                K %= N
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        currCount = 0
+        for i in range(len(s)):
+            if(s[i].isdigit()):
+                currCount+=currCount*((int(s[i])-1))
             else:
-                if K == N or K == 0: return c
-                N -= 1
+                currCount+=1
+        for i in range(len(s)-1,-1,-1):
+            if(s[i].isdigit()):
+                # print(currCount,k)
+                currCount=currCount/int(s[i])
+                k=k%currCount
+                # print(currCount,k)
+            else:
+                if(currCount==k or k==0):
+                    return s[i]
+                currCount-=1

a37204171ec8744c2e5a19f1cfa15d5ce4d0dd38
Nikilesh <115042113+n11kkk@users.noreply.github.com>
2023-10-03 15:59:19 -0500

    Time: 38 ms (75.71%), Space: 16 MB (99.72%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,3 +1,4 @@
+import math
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         # ans = 0
@@ -14,5 +15,5 @@ class Solution:
                 counts[i] = 1
         ans = 0
         for i in counts.values():
-            ans+=(i)*(i-1)//2
+            ans+=math.comb(i,2)
         return ans

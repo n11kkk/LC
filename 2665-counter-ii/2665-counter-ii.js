1f1698ad3855d8289001febc1657213d18e0344c
Nikilesh <115042113+n11kkk@users.noreply.github.com>
2023-10-04 15:28:48 -0500

    Time: 69 ms (6.96%), Space: 44.6 MB (54.34%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let x = init;
+    return{increment:()=>{
+        x++;
+        return x;
+    },
+           decrement:()=>{
+           x--;
+           return x;
+          },
+        reset:()=>{
+            x = init;
+            return x;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
